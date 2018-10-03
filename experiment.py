0d009543b81c7e389ee710ba701a69b3cba83b61
jmausolf <jgm346@gmail.com>
2018-10-02 19:51:11 -0500

    progress on creating discrete matched pair schools

--- experiment.py
+++ experiment.py
@@ -34,7 +34,7 @@ def select_ga(row):
 				 (ga['region']==proximal)) & 
 				(ga['job_type']==job_type) & 					
 				(ga['prestige']==profile[-1]))
-	print(criteria)
+	#print(criteria)
 	ga = ga.loc[criteria] 
 
 	#Random Selection of Schools Meeting Criteria
@@ -49,6 +49,9 @@ def select_ga(row):
 
 def select_ug(row):
 
+	#TODO
+	#build in a counter,
+
 	profile = row[0]
 	region = row[1]
 	school = row[2]
@@ -61,32 +64,252 @@ def select_ug(row):
 	
 	#Random Selection of Schools Meeting Criteria
 	rows = np.random.choice(ug.index.values, 1)
+	#print(rows)
 	df = ug.ix[rows]
 	df = df.drop(['region', 'profile'], axis=1)
+	#print(df)
 
 	keys = df.columns.tolist()
 	vals = df.values.tolist()[0]
+	#print(vals)
 	return vals
+
+
+
+
+
+def select_ga3(row, count):
+
+	#index = row.index
+	#print(index)
+	#print(row)
+
+	#count+=1
+
+
+	profile = row[0]
+	job_type = row[1]
+	region = row[2]
+	proximal = row[3]
+
+
+	if count == 0:
+		print("FIRST MATCHED PAIR")
+	elif count == 1:
+		print("SECOND MATCHED PAIR")
+	else:
+		pass
+
+	#Select Match from Region or Proximal Region
+	ga = pd.read_csv("ga_school_key.csv")
+	criteria = (((ga['region']==region) |
+				 (ga['region']==proximal)) & 
+				(ga['job_type']==job_type) & 					
+				(ga['prestige']==profile[-1]))
+	#print(criteria)
+	ga = ga.loc[criteria] 
+	#print(ga)
+
+
+	#Random Selection of Schools Meeting Criteria
+	rows = np.random.choice(ga.index.values, 1)
+	df = ga.ix[rows]
+	df = df.drop(['region', 'job_type', 'prestige'], axis=1)
+
+	keys = df.columns.tolist()
+	vals = df.values.tolist()[0]
+	#print(vals)
+	return vals
+	#print(df)
+	#return df
+
+
+def select_ug3(row, count):
+
+	#TODO
+	#build in a counter,
+
+	profile = row[0]
+	region = row[1]
+	school = row[2]
+
+
+	if count == 0:
+		print("FIRST MATCHED PAIR")
+	elif count == 1:
+		print("SECOND MATCHED PAIR")
+	else:
+		pass
+
+	ug = pd.read_csv("ug_school_key.csv")
+	criteria = ((ug['profile']==profile) & 
+				(ug['region']==region) & 
+				(ug['ba_school']!=school))
+	ug = ug.loc[criteria] 
+	
+	#Random Selection of Schools Meeting Criteria
+	rows = np.random.choice(ug.index.values, 1)
+	#print(rows)
+	df = ug.ix[rows]
+	df = df.drop(['region', 'profile'], axis=1)
+	#print(df)
+
+	keys = df.columns.tolist()
+	vals = df.values.tolist()[0]
+	#print(vals)
+	return vals
+
+
+
+
+
 
 
 
 def join_experiment_profiles(experiment_file):
 	ex = pd.read_csv(experiment_file)
+	#print(ex.shape)
+
+	#perhaps do it by cid, for c in cid, ret 2 vals
+
 
 	#Select GA Schools
-	ga_keys = ['department','school','school_short','school_ctyst','school_cszip','school_address','title']
-	ex[ga_keys] = ex[['profile', 'job_type', 'region', 'proximal_region']].apply(lambda row: pd.Series(select_ga(row)), axis=1)
+	ga_vals = ['department','school','school_short','school_ctyst',
+			   'school_cszip','school_address','title']
+	ga_keys = ['profile', 'job_type', 'region', 'proximal_region']
+
+	ex[ga_vals] = ex[ga_keys].apply(
+							 lambda row: pd.Series(select_ga(row)), axis=1)
+
+	print(ex)
+	print(ex.shape)
 
 	#Select UG Schools
-	ug_keys = ['ba_school', 'ba_school_short', 'ba_ctyst', 'treatment', 'prestige']
-	ex[ug_keys] = ex[['profile', 'region', 'school']].apply(lambda row: pd.Series(select_ug(row)), axis=1)
+	ug_vals = ['ba_school', 'ba_school_short', 'ba_ctyst', 
+			   'treatment', 'prestige']
+	ug_keys = ['profile', 'region', 'school']
 
+	ex[ug_vals] = ex[ug_keys].apply(
+							 lambda row: pd.Series(select_ug(row)), axis=1)
+
+
+	print(ex)
+	print(ex.shape)
 
 	profiles = join_profiles_credentials()
 	df = pd.merge(ex, profiles, on=['profile'])
-	print(df)
+	#print(df)
 	return df
 
+
+	#Currently this function runs s.t. it does all ga, then all ug, same list basis each time
+	#need to try to reorg an intermediary function that gets ga and ug for a row
+	#reorg the lamda then with a counter function
+	#if count == 0, start with base lists
+	#if count > 0, subtract ug/ga lines from list, save as tmp list, sample from this list
+	#then ga/ug will not be the same for the same profile
+
+
+
+
+def join_ex_pair(ex_df, cid):
+
+
+	count = 0
+	ex = ex_df.loc[(ex_df['cid']==cid)]
+	#print(ex.shape)
+
+	#ex1 = ex.iloc[1]
+
+
+
+	#Select GA Schools
+	ga_vals = ['department','school','school_short','school_ctyst',
+			   'school_cszip','school_address','title']
+	ga_keys = ['profile', 'job_type', 'region', 'proximal_region']
+
+
+	#Select UG Schools
+	ug_vals = ['ba_school', 'ba_school_short', 'ba_ctyst', 
+			   'treatment', 'prestige']
+	ug_keys = ['profile', 'region', 'school']
+
+
+	#TODO
+	#Now that 1 and 2 are working, do diff actions for first and second pair
+	#run first pair as is, but drop school selected school ids
+	#write new df
+	#for second df, sample from tmp df created in pair 1
+
+	pairs = []
+	for i in list(ex.index):
+		row = ex.iloc[[i]].copy()
+
+		#Grad School
+		ga_result = select_ga3(row[ga_keys].values.tolist()[0], i)
+		row[ga_vals] = pd.DataFrame([ga_result], index=row.index)
+
+		#Undergrad
+		ug_result = select_ug3(row[ug_keys].values.tolist()[0], i)
+		row[ug_vals] = pd.DataFrame([ug_result], index=row.index)
+
+		pairs.append(row)
+
+
+	df = pd.concat(pairs)
+
+	return df
+
+
+
+def join_experiment_profiles_counter(experiment_file):
+	#ex = pd.read_csv(experiment_file)
+	ex_full = pd.read_csv(experiment_file)
+
+
+
+
+	"""
+	#print(ex.iloc[[1]])
+	ex = ex_full.iloc[0:2]
+	#ex = ex_full.iloc[[0]]
+	#print(ex.shape)
+
+	#perhaps do it by cid, for c in cid, ret 2 vals
+	#ex_ci
+
+	#Select GA Schools
+	ga_vals = ['department','school','school_short','school_ctyst',
+			   'school_cszip','school_address','title']
+	ga_keys = ['profile', 'job_type', 'region', 'proximal_region']
+
+	ex[ga_vals] = ex[ga_keys].apply(
+							 lambda row: pd.Series(select_ga(row)), axis=1)
+	#ex[ga_vals] = pd.Series(select_ga(ex.iloc[[1]]))
+
+
+	#print(ex)
+	#print(ex.shape)
+
+	#Select UG Schools
+	ug_vals = ['ba_school', 'ba_school_short', 'ba_ctyst', 
+			   'treatment', 'prestige']
+	ug_keys = ['profile', 'region', 'school']
+
+	ex[ug_vals] = ex[ug_keys].apply(
+							 lambda row: pd.Series(select_ug(row)), axis=1)
+	#ex[ug_vals] = pd.Series(select_ug(ex.iloc[[1]]))
+	"""
+
+	df0 = join_ex_pair(ex_full, 'c01')
+	print(df0)
+	#print(ex)
+	#print(ex.shape)
+
+	#profiles = join_profiles_credentials()
+	#df = pd.merge(ex, profiles, on=['profile'])
+	#print(df)
+	#return df
 
 
 #join_experiment_profiles("experiment_test.csv")
@@ -143,15 +366,23 @@ def send_email_iter(row):
 
 def deploy_emails(experiment_csv):
 
-	df = join_experiment_profiles(experiment_csv)
+	#df = join_experiment_profiles(experiment_csv)
+	df = join_experiment_profiles_counter(experiment_csv)
 	#print(df)
 
-	df['metadata'] = df.apply(send_email_iter, axis=1)
-	print(df)
+	ex_out = "{}_output.csv".format(experiment_csv.split(".")[0])
+	#df.to_csv(ex_out, index=False)
+
+	#TODO
+	#split DF into day1_app1, day2_app2
+	#apply exp to day1df, wait, apply to day2df
+
+	#df['metadata'] = df.apply(send_email_iter, axis=1)
+	#print(df)
 
 	#write result file
-	outfile = "logs/results_log_{}".format(experiment_csv)
-	df.to_csv(outfile, index=False)
+	#outfile = "logs/results_log_{}".format(experiment_csv)
+	#df.to_csv(outfile, index=False)
 
 
 
