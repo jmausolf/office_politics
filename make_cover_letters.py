bf9b11d8ecaae9342c6b0cf9f5473f11d3edb6a8
jmausolf <jgm346@gmail.com>
2018-10-04 18:17:51 -0500

    got discrete internship matched pairs to render from experiment file and send successfully

--- make_cover_letters.py
+++ make_cover_letters.py
@@ -15,8 +15,6 @@ def keep_or_replace(app_company, intern_company):
 		return "KEEP"
 
 
-
-
 def sub_cover_letter_internship(app_company, job_type_prestige):
 	
 	d = job_type_prestige.copy()
@@ -37,9 +35,6 @@ def sub_cover_letter_internship(app_company, job_type_prestige):
 
 	ic_phrase = "{} and {}".format(ics[0], ics[1])
 	return ic_phrase
-
-
-
 
 
 def make_text_sig(name, title, school, phone, gmail_user):
@@ -69,11 +64,6 @@ def make_html_sig(name, title, school, phone, gmail_user):
 	return sig_html
 
 
-
-
-
-
-
 def make_text_cl(profile, 
 				job_type,
 				contact,
@@ -84,6 +74,10 @@ def make_text_cl(profile,
 				title, 
 				school,
 				department,
+				internship1,
+				int1_ctyst,
+				internship2,
+				int2_ctyst,
 				treatment,
 				phone, 
 				gmail_user,
@@ -91,10 +85,7 @@ def make_text_cl(profile,
 				):
 
 
-	ik = "{}_{}".format(job_type, profile[-1])
-	intern_key = internship_keys[ik]
-	internships = sub_cover_letter_internship(company, intern_key)
-
+	internships = "{} and {}".format(internship1, internship2)
 
 	#method_name = "{}_{}".format(profile, job_type)
 	method_name = "{}".format(job_type)
@@ -107,20 +98,16 @@ def make_text_cl(profile,
 		raise NotImplementedError("Class `{}` does not implement `{}`"
 			.format(cl().__class__.__name__, method_name))
 
-	message_body = cl_text(contact, job, office, company, internships, school, department, treatment)
+	message_body = cl_text(contact, job, office, company, 
+						   internships, school, department, treatment)
 
 	if add_sig is True:
 		message_sig = make_text_sig(name, title, school, phone, gmail_user)
 		message_text = message_body+'\n\n'+message_sig
-		#print(message_text)
 	else:
 		message_text = message_body
 
-	#print(message_text)
-	return message_text, internships
-
-
-
+	return message_text
 
 
 def make_html_text_cl(profile, 
@@ -133,20 +120,38 @@ def make_html_text_cl(profile,
 				title, 
 				school,
 				department,
+				internship1,
+				int1_ctyst,
+				internship2,
+				int2_ctyst,
 				treatment,
 				phone, 
 				gmail_user
 				):
 
 	#Make Text CL (no sig)
-	message_output = make_text_cl(profile, job_type, contact, job, office, company, name, title, school, department, treatment, phone, gmail_user, add_sig=False)
-	message_text_body = message_output[0]
-	internships = message_output[1]
+	message_text_body = make_text_cl(profile, 
+								  job_type,
+								  contact,
+								  job,
+								  office,
+								  company,
+								  name,
+								  title,
+								  school,
+								  department,
+								  internship1,
+								  int1_ctyst,
+								  internship2,
+								  int2_ctyst,
+								  treatment,
+								  phone,
+								  gmail_user,
+								  add_sig=False)
 
 	#Make Text Sig/Full Text Message
 	sig_text = make_text_sig(name, title, school, phone, gmail_user)
 	message_text = message_text_body+'\n\n'+sig_text
-	#print(message_text)
 
 	#Make HTML Sig
 	sig_html = make_html_sig(name, title, school, phone, gmail_user)
@@ -155,32 +160,8 @@ def make_html_text_cl(profile,
 	#(needed to keep the internships consistent)
 	message_html = """<html><div style="color:rgb(0,0,0);font-family:&quot;Times New Roman&quot;,Times,serif,Times,EmojiFont,&quot;Apple Color Emoji&quot;,&quot;Segoe UI Emoji&quot;,NotoColorEmoji,&quot;Segoe UI Symbol&quot;,&quot;Android Emoji&quot;,EmojiSymbols;font-size:16px;font-style:normal;font-variant-ligatures:normal;font-variant-caps:normal;font-weight:400;letter-spacing:normal;text-align:start;text-indent:0px;text-transform:none;white-space:normal;word-spacing:0px;background-color:rgb(255,255,255);text-decoration-style:initial;text-decoration-color:initial">{}\n\n{}</div></html>
 	""".format(textile.textile( message_text_body ), sig_html)
-	#print(message_html)
 
-	return message_text, message_html, internships
-
-
-
-
-"""
-def join_profiles_credentials():
-	cred = pd.read_csv("credentials.csv")
-	prof = pd.read_csv("profiles.csv")
-
-	df = pd.merge(cred, prof, on=['profile'])
-	return df
-
-def join_experiment_profiles(experiment_file):
-	experiment = pd.read_csv(experiment_file)
-	profiles = join_profiles_credentials()
-	df = pd.merge(experiment, profiles, on=['profile'])
-
-	#df['intern_key'] = internship_keys['data_science_H']
-	print(df)
-	return df
-"""
-
-
+	return message_text, message_html
 
 #join_experiment_profiles("experiment_test.csv")
 
