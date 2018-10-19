ef74fc9bd61f9835bd5644f30108d7f8953bf750
jmausolf <jgm346@gmail.com>
2018-10-19 17:18:20 -0500

    improving mbox converter to take multiple mboxes and combine them all to a single csv

--- mbox_tester/mbox_converter.py
+++ mbox_tester/mbox_converter.py
@@ -1,12 +1,30 @@
-# assumes your file is called mbox (which it is if you export from Mac Mail)
-# writes to a file called mbox.csv
-
 import mailbox
 import csv
 import re
+import sys
+import subprocess
+import argparse
+from glob import glob
+import pandas as pd
+
 
 def remove_non_ascii_2(text):
     return re.sub(r'[^\x00-\x7F]+', "", text)
+
+
+def replace_unicode_literals(text):
+	text = text.replace("\\xe2\\x80\\x90", "-")
+	text = text.replace("\\xe2\\x80\\x91", "-")
+	text = text.replace("\\xe2\\x80\\x92", "-")
+	text = text.replace("\\xe2\\x80\\x93", "-")
+	text = text.replace("\\xe2\\x80\\x94", "-")
+	text = text.replace("\\xe2\\x80\\x98", "'")
+	text = text.replace("\\xe2\\x80\\x99", "'")
+	text = text.replace("\\xe2\\x80\\x9a", "'")
+	text = text.replace("\\xe2\\x80\\x9b", "'")
+	text = text.replace("\\xe2\\x80\\x9c", '"')
+	text = text.replace("\\xe2\\x80\\x9d", '"')
+	return text
 
 
 def replace_links(link_text):
@@ -14,30 +32,100 @@ def replace_links(link_text):
 	return link
 
 
-# motherfucking recursion, because email is damn weird.
-# each payload can contain many other payloads, which can contain many *other* payloads
-# this only exports the text/plain payload, the thing you read
 def more_payloads(message):
 	body = ""
 	if message.is_multipart():
-		print("if")
 		for payload in message.get_payload():
 			body += more_payloads(payload)
 	else:
-		print("else")
 		if message.get_content_type() == 'text/plain':
 			body = message.get_payload(decode=True)
 
-	output = remove_non_ascii_2(str(body))
-	output = "\n".join(output.splitlines())
-	output = output.replace("\\r\\n", "\n").replace("\\n\\n", "\n")
-	output = output.replace('\b"', '')
-	output = replace_links(output)
-	#print(output)
-	return output
 
-with open("mbox.csv", "w") as outfile:
-	writer = csv.writer(outfile)
-	for message in mailbox.mbox('Inbox.mbox'):
+	o = remove_non_ascii_2(str(body))
+	o = "\n".join(o.splitlines())
+	o = o.replace("\\r\\n", "\n")
+	o = o.replace("\\n\\n", "\n").replace("\\n", "\n")
+	o = o.replace("\b'", '').replace("b'", '')
+	o = replace_unicode_literals(o)
+	o = replace_links(o)
+	return o
+
+
+def get_sender(message):
+	'''
+	Expects message from mbox
+	Returns sender name and email
+	'''
+
+	from_text = message['from']
+	terms = from_text.split(' <')
+	from_name = terms[0]
+	from_email = terms[1].replace('>', '')
+
+	return from_name, from_email
+
+
+def mbox_extractor(mboxfile, writer):
+	print("[*] extracting emails from mbox: {}...".format(mboxfile))
+	for message in mailbox.mbox(mboxfile):
+			
+		from_name, from_email = get_sender(message)
 		body = more_payloads(message)
-		writer.writerow([message['subject'], message['from'], message['date'], body])
+		writer.writerow([
+						from_name,
+						from_email, 
+						message['to'], 
+						message['date'], 
+						message['subject'], 
+						body,
+						mboxfile
+						])
+
+
+def csv_from_mboxes(filename='mbox.csv'):
+
+	with open(filename, "w") as outfile:
+		writer = csv.writer(outfile)
+		cols = ['from_name',
+				'from_email',
+				'to_email',
+				'date',
+				'subject', 
+				'message',
+				'mbox']
+		writer.writerow(cols)
+
+
+		mboxes = glob('*.mbox')
+		for m in mboxes:
+			mbox_extractor(m, writer)
+
+
+
+			
+if __name__=='__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-f", 
+						"--filename",
+						default='mbox.csv',
+						type=str, 
+						help="outfile name")
+	parser.add_argument("-o", 
+						"--open",
+						default=False,
+						type=bool, 
+						help="open outfile?")
+	args = parser.parse_args()
+
+	if not (args.filename or args.open):
+	    parser.error('No action requested, add argument...')
+
+
+	print("[*] Running requested tasks...")
+	csv_from_mboxes(args.filename)
+	print(pd.read_csv(args.filename))
+	if args.open is True:
+		subprocess.call("open {}".format(args.filename), shell=True)
+	print("[*] Done.")
+
