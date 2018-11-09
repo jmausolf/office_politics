9abd34843e35041c19f467f628f1a648a2d5a285
jmausolf <jgm346@gmail.com>
2018-11-09 00:04:13 -0600

    updating make exp protocol

--- make_protocol.py
+++ make_protocol.py
@@ -4,52 +4,43 @@ import pandas as pd
 
 
 #Start with Employers.CSV
-emp = pd.read_csv('employers_key.csv', converters={'office_state': str})
-print(emp)
-print(emp.dtypes)
+#print(emp)
 
 
 #0a TODO Join with Contact Name/Email by Company
 
-#0b TODO Map `region` and `proximal_region` from `office_state` col
-def get_region(df, state_col):
 
-	pass
+def get_regions(df,
+				state_col,
+				region_col='state',
+				region_key='region_key.csv'
+				):
+
+	sc = state_col
+	rc = region_col
+
+	regions = pd.read_csv(region_key)
+	df = df.merge(regions, how='left', left_on=sc, right_on=rc)
+	df.to_csv('test_regions.csv', index=False)
 	return df
 
-def get_prox_region(df, state_col):
 
-	pass
-	return df
+def make_pairs(df):
 
-def get_regions(df, state_col, region_key='region_key.csv'):
-
-	regions = pd.read_csv(region_key, converters={'state': str})
-	print(regions.dtypes)
-	print(regions)
-	x = df.merge(regions, how='left', left_on='office_state', right_on='state')
-	#x = df.merge(regions, how='left')
-	print(x)
-	#df = get_region(df, state_col)
-	#df = get_prox_region(df, state_col)
-	x.to_csv('test_regions.csv', index=False)
-
-	#TODO fix company state cleaning to strip forward whitespace
-
+	second_pair = df.copy()
+	df = df.append(second_pair, ignore_index=True)
+	df.sort_values(by=['cid'], inplace=True)
 	return df
 
 
 
 
+emp = pd.read_csv('employers_key.csv')
+emp = get_regions(emp, 'office_state')
+emp = make_pairs(emp)
+print(emp)
 
-###########
-###########
-get_regions(emp, 'office_state')
 
-
-#TODO
-#Generate Pairs (Duplicate Each Row)
-#(Sort by contact name, contact email, cid)
 
 #TODO
 #Number all rows by id, a1, a2, ...
@@ -57,3 +48,4 @@ get_regions(emp, 'office_state')
 #TODO
 #Assign a `profile` to each row through random selection
 #look at Rscript, just use that or do in Python??
+
