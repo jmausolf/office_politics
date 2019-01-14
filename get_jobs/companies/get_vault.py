3d9d50032904544ad5b131d264d60c59309f5d8c
jmausolf <jgm346@gmail.com>
2019-01-14 17:34:28 -0600

    updating source cleaning and process

--- get_jobs/companies/get_vault.py
+++ get_jobs/companies/get_vault.py
@@ -21,22 +21,23 @@ vault_keys = 	[
 
 
 def get_date():
-    #Get Date for Filenames
-    now = datetime.datetime.now()
-    date = now.strftime("%Y-%m-%d")
-    return date
+	#Get Date for Filenames
+	now = datetime.datetime.now()
+	date = now.strftime("%Y-%m-%d")
+	return date
 
 
 def rt(d):
-    times = np.random.rand(1000)+np.random.rand(1000)+d
-    return np.random.choice(times, 1).tolist()[0]
+	times = np.random.rand(1000)+np.random.rand(1000)+d
+	return np.random.choice(times, 1).tolist()[0]
 
 
 def scroll_page(n, delay=0.25):
 	scroll = 0
 	for i in range(1, n+1):
 		scroll +=1
-		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+		js = "window.scrollTo(0, document.body.scrollHeight);"
+		driver.execute_script(js)
 		time.sleep(rt(delay))
 
 
@@ -57,6 +58,17 @@ def get_vault_urls(vault_keys):
 	return urls
 
 
+def extract_source(url):
+	stem = 'http://www.vault.com/company-rankings/'
+	vault_list = url.split(stem)[1].split('/')[1]
+	if vault_list == 'best-boutique-consulting-firms':
+		vault_list = 'vault-'+vault_list
+	else:
+		pass
+
+	return vault_list
+
+
 def extract_type(url):
 	stem = 'http://www.vault.com/company-rankings/'
 	cid_type = url.split(stem)[1].split('/')[0]
@@ -65,19 +77,27 @@ def extract_type(url):
 
 def get_companies(url):
 
-	print('[*] extracting jobs for Vault url: {}'.format(url))
+	print('[*] extracting companies for Vault url: {}'.format(url))
 
 	d.get(url)
 
+	#Companies
 	pat = "//td[@class='infoColumn']"
-	rank_loc = "xpath", pat
-	ranks = d.find_elements(*rank_loc)
-	companies = [c.find_element_by_css_selector('h3').text for c in ranks]
+	c_loc = "xpath", pat
+	c_elm = d.find_elements(*c_loc)
+	companies = [c.find_element_by_css_selector('h3').text for c in c_elm]
+
+	#Ranks
+	pat = "//td[@class='rankColumn']"
+	r_loc = "xpath", pat
+	r_elm = d.find_elements(*r_loc)
+	ranks = [r.find_element_by_css_selector('span').text for r in r_elm]
 
 	#Make Data Frame
-	df = pd.DataFrame({'company':companies})
+	df = pd.DataFrame({'company':companies, 'rank':ranks})
 
 	#Add Company Type
+	df['source'] = extract_source(url)
 	df['job_type'] = extract_type(url)
 	return df
 
@@ -104,16 +124,16 @@ def main(vault_keys, out='vault_jobs'):
 
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--output", default='vault_jobs', type=str)
-    args = parser.parse_args()
-    
-    #Start Vault Scraper
-    starttime=time.time()
-    d = webdriver.Firefox()
-    d.implicitly_wait(0)
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-o", "--output", default='vault_jobs', type=str)
+	args = parser.parse_args()
+	
+	#Start Vault Scraper
+	starttime=time.time()
+	d = webdriver.Firefox()
+	d.implicitly_wait(0)
 
-    #Main Vault Scraper
-    main(vault_keys, args.output)          
-    time.sleep(rt(5))
-    d.close()
+	#Main Vault Scraper
+	main(vault_keys, args.output)		  
+	time.sleep(rt(5))
+	d.close()
