8960e496f41c204befca5cf30649f974ba2c1daa
jmausolf <jgm346@gmail.com>
2019-02-21 17:00:37 -0600

    adding cs templates

--- cover_letters.py
+++ cover_letters.py
@@ -49,7 +49,7 @@ class cl(object):
 	def computer_science_A(self, contact, contact_last_name, job, office, company, internships, school, department, treatment):
 		message = inspect.cleandoc("""Dear {0},
 
-			I am writing in response to your notice for the {1} position at your {2} office. I am a masters candidate in {6} at {5}, specializing in the application of recurrent neural networks in cloud computing. {3} is a leader in data science and artificial intelligence, and I am confident, together, we would be a great match.
+			I am writing in response to your notice for the {1} position at your {2} office. I am a masters candidate in {6} at {5}, specializing in the application of recurrent neural networks in cloud computing. {3} is a leader in software development, and I am confident, together, we would be a great match.
 
 			As a computer scientist, I have both the theoretical knowledge and applied experience to make a difference at {3}. As you can see from my resume, I have not only developed enhanced RNN algorithms in Java, but also used Python, Scala, and SQL to apply deep neural nets and streamline ETL pipelines as a software engineering intern for both {4}. Collectively, my background in computer science, statistics, and mathematical modeling gives me first-hand experience into the crux of today’s complex puzzles in data science and their applications at the frontier of artificial intelligence.
 
@@ -67,7 +67,7 @@ class cl(object):
 		contact_full = "{} {}".format(contact, contact_last_name)
 		message = inspect.cleandoc("""Dear {0}:
 
-			I hope this email finds you well. I recently came across the {1} position at {3}'s {2} office. As a M.S. candidate in {6} at {5}, I research the application of nonparametric bound estimation for deep reinforcement learning, a type of computer vision. Given, {3}'s opportunities in machine learning and data science, I would love to contribute my talents.
+			I hope this email finds you well. I recently came across the {1} position at {3}'s {2} office. As an M.S. candidate in {6} at {5}, I research the application of nonparametric bound estimation for deep reinforcement learning, a type of computer vision. Given, {3}'s opportunities in engineering cutting edge software, I would love to contribute my talents.
 
 			With my background in computer science, I exhibit both the academic theory and pragmatic qualifications to be impactful at {3}. As evidenced in my resume, I have used my dissertation to develop a C++ library that optimizes deep learning. Moreover, I have applied my computational skills in Python, SQL, and Hadoop to improve ETL server efficiency and provide impactful analytics as a summer software engineering intern for both {4}. Both within and outside the workplace, I embrace collaboration, such as my efforts at {4} to share actionable data solutions or my past initiatives as {7} to direct fundraisers and organize student activities.
 
