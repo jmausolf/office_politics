184062e8a6504eac2ec82fd068aaa4689dc1e1b6
jmausolf <jgm346@gmail.com>
2019-03-23 12:56:44 -0500

    updates to prep for leadiro

--- get_jobs/companies/prep_master.py
+++ get_jobs/companies/prep_master.py
@@ -3,12 +3,11 @@ import numpy as np
 import pandas as pd
 import re
 from string import punctuation
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
 import warnings
 import itertools
 import glob
 from replace_dict import *
+
 
 def remove_non_ascii_2(text):
     return re.sub(r'[^\x00-\x7F]+', "", text)
@@ -215,22 +214,23 @@ def modify_csv(csv_file, path='source/', dest='clean/', outfile=True, replace=Fa
 def clean_fortune_1000(source_file):
     clean_csv = modify_csv(source_file)
     df = pd.read_csv(clean_csv)
+    df['raw_company'] = df['company']
     df['source'] = 'fortune1000'
-    df['job_type'] = 'data_science'
+    df['job_type'] = 'data_science' 
 
     #Make Id
     df['index'] = df.index
     df['index'] = df['index'].apply(lambda x: str(x))
     df['list_id'] = 'f1000_'+df['index']
-    df = df[['list_id', 'company', 'rank', 'source', 'job_type']]
+    df = df[['list_id', 'company', 'raw_company', 'rank', 'source', 'job_type']]
     df.to_csv(clean_csv, index=False)
     return df
 
 ## Clean Hedge Fund List
 def clean_hedge(source_file):
     clean_csv = modify_csv(source_file)
-
     df = pd.read_csv(clean_csv)
+    df['raw_company'] = df['company']
     df['source'] = 'hedge_fund_100'
     df['job_type'] = 'quant'
 
@@ -238,7 +238,7 @@ def clean_hedge(source_file):
     df['index'] = df.index
     df['index'] = df['index'].apply(lambda x: str(x))
     df['list_id'] = 'hedge_'+df['index']
-    df = df[['list_id', 'company', 'rank', 'source', 'job_type']]
+    df = df[['list_id', 'company', 'raw_company', 'rank', 'source', 'job_type']]
     df.to_csv(clean_csv, index=False)
     return df
 
@@ -247,12 +247,13 @@ def clean_hedge(source_file):
 def clean_vault(source_file):
     clean_csv = modify_csv(source_file)
     df = pd.read_csv(clean_csv)
+    df['raw_company'] = df['company']
 
     #Make Id
     df['index'] = df.index
     df['index'] = df['index'].apply(lambda x: str(x))
     df['list_id'] = 'vault_'+df['index']
-    df = df[['list_id', 'company', 'rank', 'source', 'job_type']]
+    df = df[['list_id', 'company', 'raw_company', 'rank', 'source', 'job_type']]
     df.to_csv(clean_csv, index=False)
     return df
 
@@ -264,6 +265,7 @@ def clean_nasdaq(source_file):
     df = clean_col_names(df)
 
     #Remove Duplicate Companies (Some Have Multiple Symbols)
+    df['raw_company'] = df['name']
     df = df.drop_duplicates('name')
 
     #Remove If MarketCap is Zero, Then Rank by Marketcap
@@ -280,7 +282,7 @@ def clean_nasdaq(source_file):
 
     #Assign ID
     df['list_id'] = 'nasdaq_'+df['symbol']
-    keep_cols = ['list_id', 'company', 'rank']
+    keep_cols = ['list_id', 'company', 'raw_company', 'rank']
     df = df[keep_cols]
     df['source'] = 'nasdaq_tech'
     df['job_type'] = 'data_science_b'
@@ -294,12 +296,13 @@ def clean_nasdaq(source_file):
 def clean_glassdoor(source_file):
     clean_csv = modify_csv(source_file)
     df = pd.read_csv(clean_csv)
+    df['raw_company'] = df['company']
 
     #Make Id
     df['index'] = df.index
     df['index'] = df['index'].apply(lambda x: str(x))
     df['list_id'] = 'glassdoor_'+df['index']
-    df = df[['list_id', 'company', 'rank', 'source', 'job_type']]
+    df = df[['list_id', 'company', 'raw_company', 'rank', 'source', 'job_type']]
     df.to_csv(clean_csv, index=False)
     return df
 
@@ -308,12 +311,13 @@ def clean_glassdoor(source_file):
 def clean_forbes(source_file):
     clean_csv = modify_csv(source_file)
     df = pd.read_csv(clean_csv)
+    df['raw_company'] = df['company']
 
     #Make Id
     df['index'] = df.index
     df['index'] = df['index'].apply(lambda x: str(x))
     df['list_id'] = 'forbes_'+df['index']
-    df = df[['list_id', 'company', 'rank', 'source', 'job_type']]
+    df = df[['list_id', 'company', 'raw_company', 'rank', 'source', 'job_type']]
     df.to_csv(clean_csv, index=False)
     return df
 
@@ -322,6 +326,7 @@ def clean_forbes(source_file):
 def clean_cnbc(source_file):
     clean_csv = modify_csv(source_file)
     df = pd.read_csv(clean_csv)
+    df['raw_company'] = df['company']
 
     #Add Columns
     df['source'] = 'cnbc_disruptor50'
@@ -331,7 +336,7 @@ def clean_cnbc(source_file):
     df['index'] = df.index
     df['index'] = df['index'].apply(lambda x: str(x))
     df['list_id'] = 'cnbc_'+df['index']
-    df = df[['list_id', 'company', 'rank', 'source', 'job_type']]
+    df = df[['list_id', 'company', 'raw_company', 'rank', 'source', 'job_type']]
     df.to_csv(clean_csv, index=False)
     return df
 
@@ -340,12 +345,13 @@ def clean_cnbc(source_file):
 def clean_bi(source_file):
     clean_csv = modify_csv(source_file)
     df = pd.read_csv(clean_csv)
+    df['raw_company'] = df['company']
 
     #Make Id
     df['index'] = df.index
     df['index'] = df['index'].apply(lambda x: str(x))
     df['list_id'] = 'bi_'+df['index']
-    df = df[['list_id', 'company', 'rank', 'source', 'job_type']]
+    df = df[['list_id', 'company', 'raw_company', 'rank', 'source', 'job_type']]
     df.to_csv(clean_csv, index=False)
     return df
 
@@ -358,6 +364,7 @@ def clean_ru3000(source_file):
 
     #Remove Duplicate Companies (Some Have Multiple Symbols)
     df = df.drop_duplicates('company')
+    df['raw_company'] = df['company']
 
     #Replace & Co
     rep_and_co1 = lambda x: re.sub(r'\&\sCO\s', ' & COMPANY ', x)
@@ -448,7 +455,7 @@ def clean_ru3000(source_file):
 
     #Assign ID
     df['list_id'] = 'ru_'+df['ticker']
-    keep_cols = ['list_id', 'company', 'rank']
+    keep_cols = ['list_id', 'company', 'raw_company', 'rank']
     df = df[keep_cols]
     df['source'] = 'ru3000'
     df['job_type'] = 'data_science_b'
@@ -542,11 +549,13 @@ prep_df['company'] = prep_df['company'].replace(company_replacements)
 prep_df['company_lower'] = prep_df['company'].apply(lambda x: x.lower())
 prep_df.drop_duplicates('company_lower', inplace=True)
 prep_df = prep_df.drop(['company_lower'], axis=1)
+
+
+#Make Leadiro Specific File and Drop Raw Company
+leadiro_abm = prep_df[['list_id', 'company', 'raw_company']]
+leadiro_abm.to_csv('../../leadiro/leadiro_master_abm.csv', index=False)
+prep_df = prep_df.drop(['raw_company'], axis=1)
 print(prep_df)
-
-
-
-
 
 
 
@@ -554,18 +563,6 @@ print(prep_df)
 ## Make Multiple Job Types
 #############################################
 
-#Key = job_type (first one)
-'''
-job_types_dict = {'data_science':['data_science', 'stats', 'quant', 'computer_science', 'mba', None],
-                  'quant':['quant', 'data_science', 'stats', 'computer_science', 'mba', None],
-                  'banking':['quant', 'data_science', 'mba', 'stats', 'computer_science', None],
-                  'accounting':['mba', 'stats', 'data_science', 'computer_science', 'quant', None],
-                  'consulting':['data_science', 'computer_science', 'consultant', 'mba', 'stats', 'quant'],
-                  'law':['mba', 'data_science', 'computer_science', 'stats', 'quant', None]
-}
-'''
-
-'''
 #Full Job Types
 job_types_dict = {'data_science':['data_science', 'computer_science', 'mba', 'mba_finance', 'stats', 'quant', 'mba_analyst'],
                   'quant':['quant', 'data_science', 'mba_finance', 'computer_science', 'stats', 'mba', 'mba_analyst'],
@@ -575,9 +572,10 @@ job_types_dict = {'data_science':['data_science', 'computer_science', 'mba', 'mb
                   'law':['mba', 'mba_finance', 'data_science', 'computer_science', 'stats', 'quant', 'mba_analyst'],
                   'data_science_b':['data_science', 'computer_science', 'mba', 'mba_finance', 'stats', 'quant', 'mba_analyst']
 }
-'''
+
 
 #MBA Test
+'''
 job_types_dict = {'data_science':['mba', 'mba_finance', 'mba_analyst'],
                   'quant':['mba', 'mba_finance', 'mba_analyst'],
                   'banking':['mba', 'mba_finance', 'mba_analyst'],
@@ -586,6 +584,7 @@ job_types_dict = {'data_science':['mba', 'mba_finance', 'mba_analyst'],
                   'law':['mba', 'mba_finance', 'mba_analyst'],
                   'data_science_b':['mba', 'mba_finance', 'mba_analyst']
 }
+'''
 
 
 def job_col_names(job_types_dict, first_key):
@@ -621,148 +620,3 @@ make_job_search_cols(prep_df, job_types_dict)
 
 
 
-
-
-#############################################
-## Duplicate Scrap
-#############################################
-
-
-'''
-def show_possible_duplicates(contains_dupes, threshold=70, scorer=fuzz.token_set_ratio):
-    """
-    A Modified Version of fuzzywuzzy process.dedupe:
-    https://github.com/seatgeek/fuzzywuzzy/blob/master/fuzzywuzzy/process.py
-
-    Code is Modified to Return Possible Duplicates to Review and Investigate
-    (Original code dropped too many valid non-duplicates even with a high threshold
-    and various scorers or conversely missed true duplicates).
-
-    Rather than take the "first item as our 'canonical example'",
-        possible duplicate pairs are sorted and aggregated, then displayed.
-
-    Args:
-        contains_dupes: A list of strings that we would like to dedupe.
-        threshold: the numerical value (0,100) point at which we expect to find duplicates.
-            Defaults to 70 out of 100
-        scorer: Optional function for scoring matches between the query and
-            an indiv
-            ual processed choice. This should be a function
-            of the form f(query, choice) -> int.
-            By default, fuzz.token_set_ratio() is used and expects both query and
-            choice to be strings.
-
-
-        """
-
-    extractor = []
-    possibles = []
-
-    # iterate over items in *contains_dupes*
-    for item in contains_dupes:
-        # return all duplicate matches found
-        matches = process.extract(item, contains_dupes, limit=None, scorer=scorer)
-        #print(matches)
-        # filter matches based on the threshold
-        filtered = [x for x in matches if x[1] > threshold]
-        #print(filtered)
-        # if there is only 1 item in *filtered*, no duplicates were found so append to *extracted*
-        if len(filtered) == 1:
-            extractor.append(filtered[0][0])
-
-        else:
-            # alpha sort
-            filtered = sorted(filtered, key=lambda x: x[0])
-            #print(filtered)
-            # length sort
-            filter_sort = sorted(filtered, key=lambda x: len(x[0]), reverse=True)
-            #print(filter_sort)
-            # take first item as our 'canonical example'
-            extractor.append(filter_sort[0][0])
-            possibles_no_score = [x[0] for x in filter_sort]
-            possibles.append(possibles_no_score)
-
-    # uniquify *extractor* list
-    keys = {}
-
-
-    def sort_lists_by_length(llists):
-        l_lengths = [(l, len(l)) for l in llists]
-        sorted_ll = sorted(l_lengths, key=lambda x: x[1], reverse=True)
-        return [l[0] for l in sorted_ll]
-
-    #Start with a set sorted by length
-    possibles = sort_lists_by_length(possibles)
-    print(possibles)
-    print(len(possibles))
-
-    check_dupes = set()
-    possible_dupes = []
-
-    for sl in possibles:
-        first_item = sl[0]
-        i = 0
-        for item in sl:
-            i+=1
-            if item not in check_dupes and i == 1:
-                possible_dupes.append(sl)
-                check_dupes.add(item)
-            elif item not in check_dupes and i > 1:
-                check_dupes.add(item)
-
-
-    print(possible_dupes)
-    print(len(possible_dupes))
-
-    dupes = pd.Series(possible_dupes)
-    dupes.name = 'possible_dupes'
-    dupes = pd.DataFrame(dupes)
-    print(dupes)
-
-
-    for e in extractor:
-        keys[e] = 1
-    extractor = keys.keys()
-
-    # check that extractor differs from contain_dupes (e.g. duplicates were found)
-    # if not, then return the original list
-    if len(extractor) == len(contains_dupes):
-        return contains_dupes
-    else:
-        return extractor
-
-def dedupe_fuzzy(df, col):
-
-    #Data Column to Dedupe
-    to_dedupe = df[col].tolist()[0:500]
-
-
-    #result =  list(dedupe2(to_dedupe, threshold=82, scorer=fuzz.WRatio))
-    #result =  list(dedupe2(to_dedupe, threshold=82, scorer=fuzz._token_set))
-    result =  list(dedupe2(to_dedupe, threshold=80))
-    #result2 =  list(dedupe2(to_dedupe, threshold=82, scorer=fuzz.WRatio))
-
-    #result = process.default_scorer(s1, s2)
-    dupes = list(set(to_dedupe).symmetric_difference(set(result)))
-    print(dupes)
-    print(len(dupes))
-
-    #print(len(result))
-    #print(type(result))
-    #print(result)
-
-
-
-#df = pd.read_csv('fortune1000-list.csv')
-#df = pd.read_csv('master_companies_prep.csv')
-#df = pd.read_csv('hedge_fund_100_institutional_investors_alpha.csv')
-#print(df.shape)
-#print(df)
-#df['company'] = df.apply(clean_company, axis=1)
-#dedupe_fuzzy(df, 'company')
-
-#df.drop_duplicates('company', inplace=True)
-#df = df.reset_index(drop=True)
-#print(df.shape)
-#df.to_csv("hedge_test2.csv", index=False)
-'''
