bf0ad1e9a62927e0b992836b5c8801cd737abd90
jmausolf <jgm346@gmail.com>
2019-03-23 22:54:59 -0500

    adjusting cs cl

--- cover_letters.py
+++ cover_letters.py
@@ -10,7 +10,7 @@ class cl(object):
 	def data_science_A(self, contact, contact_last_name, job, office, company, internships, school, department, treatment, internship1, internship2):
 		message = inspect.cleandoc("""Hi {0},
 
-			I am writing in response to your notice for the {1} opening at your {2} office. I am a doctoral candidate in {6} at {5} specializing in the application of recurrent neural networks in cloud computing. {3} has excellent career prospects in data science and artificial intelligence, and I am confident, together, we would be a great match.
+			I am writing in response to your notice for the {1} opening at your {2} office. I am a doctoral candidate in {6} at {5} specializing in the application of recurrent neural networks in cloud computing. {3} has excellent careers in data science and artificial intelligence, and I am confident, together, we would be a great match.
 
 			As a computer scientist, I have both the theoretical knowledge and applied experience to make a difference at {3}. If you peruse my resume, you'll notice that I have not only developed enhanced RNN algorithms in Java, but also used Python, Spark, and SQL to apply deep neural nets and streamline ETL pipelines as a data science intern for both {4}. Collectively, my background in computer science as well as statistical and mathematical modeling gives me first-hand experience into the crux of today’s complex puzzles in data science and their applications at the frontier of artificial intelligence.
 
@@ -44,7 +44,7 @@ class cl(object):
 	def stats_A(self, contact, contact_last_name, job, office, company, internships, school, department, treatment, internship1, internship2):
 		message = inspect.cleandoc("""Hi {0},
 
-			I am writing in response to your notice for the {1} opening at your {2} office. I am a doctoral candidate in {6} at {5} specializing in the application Bayesian nonparametric inference. {3} has excellent career prospects in statistical learning, and I am confident, together, we would be a great match.
+			I am writing in response to your notice for the {1} opening at your {2} office. I am a doctoral candidate in {6} at {5} specializing in the application Bayesian nonparametric inference. {3} has excellent careers in statistical learning, and I am confident, together, we would be a great match.
 
 			As a statistician, I have both the theoretical knowledge and applied experience to make a difference at {3}. If you peruse my resume, you'll notice that I have not only developed an enhanced Bayesian algorithm to improve efficiency in parallel computing, but I have also used Python and R to apply Bayesian and other statistical learning models as a statistical research intern at {8} and a data science intern at {9}. Collectively, my background in statistics and mathematics gives me first-hand experience into the crux of today’s complex puzzles in statistics and their applications at the frontier of both research and business intelligence.
 
@@ -79,7 +79,7 @@ class cl(object):
 	def quant_A(self, contact, contact_last_name, job, office, company, internships, school, department, treatment, internship1, internship2):
 		message = inspect.cleandoc("""Hi {0},
 
-			I am writing in response to your notice for the {1} opening at your {2} office. I am a doctoral candidate in {6} at {5} specializing in the application of Bayesian nonparametric inference in statistical learning. {3} has excellent career prospects in statistics and mathematical finance, and I am confident, together, we would be a great match.
+			I am writing in response to your notice for the {1} opening at your {2} office. I am a doctoral candidate in {6} at {5} specializing in the application of Bayesian nonparametric inference in statistical learning. {3} has excellent careers in statistics and mathematical finance, and I am confident, together, we would be a great match.
 
 			As a statistician, I have both the theoretical knowledge and applied experience to make a difference at {3}. If you peruse my resume, you'll notice that I have not only developed an enhanced Bayesian algorithm to improve efficiency in parallel computing, but I have also used Python, R, and SQL to apply Bayesian and machine learning models to simulate portfolio returns and optimize alpha as a quantitative research intern at both {4}. Collectively, my background in statistics, computation, and mathematics gives me first-hand experience into the crux of today’s complex puzzles in statistical learning and their applications at the frontier of quantitative finance.
 
@@ -113,11 +113,11 @@ class cl(object):
 	def computer_science_A(self, contact, contact_last_name, job, office, company, internships, school, department, treatment, internship1, internship2):
 		message = inspect.cleandoc("""Hi {0},
 
-			I am writing in response to your notice for the {1} opening at your {2} office. I am a masters candidate in {6} at {5} specializing in the application of recurrent neural networks in cloud computing. {3} has excellent career prospects in software development, and I am confident, together, we would be a great match.
+			I am writing in response to your notice for the {1} opening at your {2} office. I am a masters candidate in {6} at {5} specializing in the application of recurrent neural networks in cloud computing. {3} has excellent careers in software development, and I am confident, together, we would be a great match.
 
-			As a computer scientist, I have both the theoretical knowledge and applied experience to make a difference at {3}. If you peruse my resume, you'll notice that I have not only developed enhanced RNN algorithms in Java, but also used Python, Spark, and SQL to apply deep neural nets and streamline ETL pipelines as a software engineering intern for both {4}. Collectively, my background in computer science as well as statistical and mathematical modeling gives me first-hand experience into the crux of today’s complex puzzles in data science and their applications at the frontier of artificial intelligence.
+			As a computer scientist, I have both the theoretical knowledge and applied experience to make a difference at {3}. If you peruse my resume, you'll notice that I have not only developed enhanced RNN algorithms in Java, but also used Python, Spark, and SQL to apply deep neural nets and streamline ETL pipelines as a software engineering intern for both {4}. Collectively, my background in computer science as well as statistical modeling gives me first-hand experience into the crux of today’s complex puzzles in software engineering their applications at the frontier of artificial intelligence.
 
-			Although I have a number of methodological strengths and my masters degree underscores my ability to tackle multifaceted problems, I also strive to work as a team player, whether it's by working with colleagues at {4} to communicate data-driven solutions or spearheading fundraising initiatives and leading a diverse set of students during my tenure as {7}. I think you will agree that my programming background—combined with my outgoing charisma and penchant for team leadership—makes me a valuable recruit for the data scientist position at {3}.
+			Although I have a number of methodological strengths and my masters degree underscores my ability to tackle multifaceted problems, I also strive to work as a team player, whether it's by working with colleagues at {4} to communicate data-driven solutions or spearheading fundraising initiatives and leading a diverse set of students during my tenure as {7}. I think you will agree that my programming background—combined with my outgoing charisma and penchant for team leadership—makes me a valuable recruit for the position at {3}.
 
 			{0}, I am excited about this opportunity at {3} and eager to discuss next steps. Attached, please find a copy of my resume. I look forward to speaking with you soon so that we can discuss the position further.
 
@@ -131,11 +131,11 @@ class cl(object):
 		contact_full = "{} {}".format(contact, contact_last_name)
 		message = inspect.cleandoc("""Dear {0}:
 
-			I hope this email finds you well. I recently came across the {1} position at {3}'s {2} office. As an M.S. candidate in {6} at {5}, I research the application of nonparametric bound estimation for deep reinforcement learning, a type of computer vision. Given, {3}'s opportunities in engineering cutting edge software, I would love to contribute my talents.
+			I hope this email finds you well. I recently came across the {1} position at {3}'s {2} office. As an M.S. candidate in {6} at {5}, I research the application of nonparametric bound estimation for deep reinforcement learning, a type of computer vision. Given, {3}'s opportunities in computer programming, I would love to contribute my talents.
 
 			With my background in computer science, I exhibit both the academic theory and pragmatic qualifications to be impactful at {3}. As evidenced in my resume, I have used my dissertation to develop a C++ library that optimizes deep learning. Moreover, I have applied my computational skills in Python, SQL, and Hadoop to improve ETL server efficiency and provide impactful analytics as a summer software engineering intern for both {4}. Both within and outside the workplace, I embrace collaboration, such as my efforts at {4} to share actionable data solutions or my past initiatives as {7} to direct fundraisers and organize student activities.
 
-			In combination, my collaborative skills and computational abilities in artificial intelligence, mathematics, and statistics illustrate the value I can bring to {3}, and I am delighted to submit my application. To that end, I have attached my resume for review. I hope to hear from you shortly.
+			In combination, my collaborative skills and computational abilities in artificial intelligence and computer programming illustrate the value I can bring to {3}, and I am delighted to submit my application. To that end, I have attached my resume for review. I hope to hear from you shortly.
 
 			Sincerely,
 			""".format(contact_full, job, office, company, internships, school, department, treatment))
