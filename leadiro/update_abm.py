a657b8e153efb8747bb4d764532b19936ddee7ae
jmausolf <jgm346@gmail.com>
2019-03-26 23:08:26 -0500

    function to update abm

---
+++ leadiro/update_abm.py
@@ -0,0 +1,52 @@
+from leadiro_contacts.clean_leadiro import *
+from lead_utils.lead_utils import *
+import os, sys, subprocess, time
+
+n = 5
+
+#Run Main Leadiro Matching
+'''
+clean_leadiro_main(
+	combined_leadiro_file='leadiro_contacts/leadiro_combined.csv',
+	leadiro_col = 'company',
+	emp_file = '../keys/employers_key.csv',
+	emp_col = 'company',
+	local_out = 'leadiro_matched.csv',
+	matched_leadiro_key = '../keys/leadiro_matched_key.csv',
+	match_only=True
+	)
+'''
+
+#Combine Leadiro Files, Clean, and Match
+os.chdir('leadiro_contacts')
+subprocess.call('python3 clean_leadiro.py', shell=True)
+os.chdir('..')
+
+
+#Calculate Remaining Leadiro Contacts
+abm_start = ret_abm_start()
+start_message = '[*] initial leadiro list: {}'.format(abm_start.shape[0])
+print(start_message)
+leads_remaining = get_leadiro_remaining()
+
+
+#Rematch to look for dupes
+'''
+clean_leadiro_main(
+	combined_leadiro_file='leadiro_contacts/leadiro_combined.csv',
+	leadiro_col = 'company',
+	emp_file = 'lead_utils/leadiro_remaining_raw.csv',
+	emp_col = 'company',
+	local_out = 'leadiro_matched.csv',
+	matched_leadiro_key = 'lead_utils/leadiro_matched_key.csv',
+	match_only=True
+	)
+'''
+
+
+abm_updated = update_abm(leads_remaining, abm_start)
+end_message = '[*] remaining leadiro firms: {}'.format(abm_updated.shape[0])
+print(end_message)
+print('[*] opening new abm list in {} seconds'.format(n))
+countdown(n)
+subprocess.call('open lead_utils/updated_abm.csv', shell=True)
