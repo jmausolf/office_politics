0e8606f94d48b5b74bd1d9124b1f970f6f92c7af
jmausolf <jgm346@gmail.com>
2019-03-30 17:53:41 -0500

    updates to emp keys and leadiro replacements

--- leadiro/replace_dict.py
+++ leadiro/replace_dict.py
@@ -4,7 +4,6 @@
 emp_key_adjustments = {
 	'J.Crew Group':'J. Crew Group',
 	'ICF':'ICF International',
-	'NIC':'NIC, Inc.',
 	'Rose':'Rose International',
 	'XO':'XO Group Inc',
 	'RGA':'Reinsurance Group of America',
@@ -20,11 +19,37 @@ emp_key_adjustments = {
 	'Marcus':'Marcus Corporation',
 	'Waddell and Reed':'Waddell & Reed Financial',
 	'HCA':'HCA Healthcare',
-
+	'CBRE Group':'Cbre',
+	'First Merchants':'First Merchants Corporation',
+	'Lululemon Athletica':'lululemon athletica',
+	'Hershey Company':'Hershey',
+	'Hca Healthcare':'HCA Healthcare',
+	'Air Products & Chemicals':'Air Products',
+	'Deere':'John Deere',
+	'Automatic Data Processing':'ADP',
+	'Automatic Data Process':'ADP',
+	'International Business Machines':'IBM',
 
 }
 
 
+emp_key_blacklist = {
+	'HCI':None,
+	'M/I Homes':None,
+	'Invitation Homes':None,
+	'TRI Pointe Group':None,
+	'Barnes':None,
+	'Merchants Bank':None,
+	'Cooper Companies':None,
+	'Crown':None,
+	'Cooper':None,
+	'Southern':None,
+	'PVH':None,
+
+
+	
+
+}
 
 
 
@@ -58,12 +83,82 @@ leadiro_changes = {
 	'Ericsson Inc. (Ericsson GE MBL Communications)':'Ericsson',
 	'Hd Supply, Inc. (Hd Supply Global Support Ctr)':'HD Supply',
 	'Snap Inc. (Snapchat)':'Snap',
+	'Basic Energy Services, Inc. (Basic)':'Basic Energy Services',
+	'Total System Services, Inc. (TSYS)':'Total System Services',
+	'Dataminr, Inc. (Ebh Enterprises, Inc.)':'Dataminr',
+	'Icims, Inc. (Icims.com)':'iCIMS',
+	'Dropbox, Inc.':'Dropbox',
+	'Servicenow, Inc. (SERVICENOW)':'ServiceNow',
+	'Smartronix, Inc.':'Smartronix',
+	'Ormat Technologies, Inc. (Ormat)':'Ormat Technologies',
+	'Compass Minerals International, Inc. (Compass Minerals)':'Compass Minerals',
+	'Atn International':'ATN',
+	'Atn International, Inc.':'ATN',
+	'Rayonier Inc.':'Rayonier',
+	'United Services Automobile Association (USAA)':'USAA',
+	'Stripe, Inc. (Stripe Payments Company)':'Stripe',
+	'Western Alliance Bank (Alliance Bank of Arizona)':'Western Alliance Bank',
+	'Jeld-Wen, Inc. (Jeld Wen International Supply)':'Jeld-Wen',
+	'Tenable, Inc. (Tenable Network Security)':'Tenable',
+	'Matson, Inc. (Matson Construction)':'Matson',
+	'Tempur Sealy International, Inc. (TEMPUR SEALY INTERNATIONAL)':'Tempur Sealy',
+	'Titan International, Inc.':'Titan',
+	'Retrophin, Inc.':'Retrophin',
+	'Aqua America, Inc. (AQUA)':'Aqua America',
+	'Wesco Inc. (On Your Way)':'Wesco',
+	'Cubesmart (Cubessmart Self Stor Logistics)':'Cubesmart',
+	'Nine Energy Service, Inc. (Nine)':'Nine Energy Service',
+	'Caleres, Inc. (FAMOUS FOOTWEAR)':'Caleres',
+	'Garmin International, Inc.':'Garmin',
+	'Syntel, Inc. (Atos Syntel)':'Syntel',
+	'Haynes International, Inc.':'Haynes',
+	'Fanatics, Inc. (Football Fanatics)':'Fanatics',
+	'Verastem, Inc. (Verastem Oncology)':'Verastem',
+	'Guess , Inc. (GUESS?)':'Guess',
+	'Amazon Web Services, Inc.':'Amazon',
+	'Zillow, Inc. (Zillow.com)':'Zillow',
+	'Air Products and Chemicals, Inc. (Air Products)':'Air Products',
+	'Mettler-Toledo International Inc.':'Mettler Toledo',
+	'Plante & Moran, Pllc':'Plante Moran',
+	'Healthcare Services Group Inc (Healthcare)':'Healthcare Services Group, Inc.',
+	'Johnson Outdoors Inc.':'Johnson Outdoors',
+	'E. I. Du Pont De Nemours and Company (Dupont)':'DuPont',
+	'Weyerhaeuo':'Weyerhaeuser',
+	'Jabil Circuit, Inc. (JABIL CIRCUIT, INC.)':'Jabil',
+	'Astronics Advanced Electronic Systems Corp. (Astronics-Aes)':'Astronics',
+	'CBS Corporation':'CBS',
+	'The Hartford Financial Services Group Inc (Hartford, The)':'The Hartford',
+	'The Progressive Corporation (PROGRESSIVE INSURANCE)':'Progressive',
+	'Celanese Americas LLC (Kep Americas)':'Celanese',
+	'Amphenol Corporation (Amphenol Rf)':'Amphenol',
+	'Automatic Data Processing, Inc. (ADP)':'ADP',
+	'International Business Machines Corporation (IBM)':'IBM',
+	'LG&e and Ku Energy LLC (PPL)':'PPL',
+	'Vf Jeanswear Limited Partnership (Vf)':'VF',
+	'Ball Aerospace & Technologies Corporation (Ball)':'Ball',
+	'Heinz Kraft Foods Company':'Kraft Heinz',
+	'Agile Enterprise Solutions, Inc. (AES)':'AES',
+	'Archer-Daniels-Midland Company (ADM)':'Archer Daniels Midland',
+	'Becton, Dickinson and Company (B D)':'Becton, Dickinson and Company (B D)',
+
+
+
+
+
 
 }
 
 
 blacklist_companies = {
 	'Speedway LLC':None,
-	#'Williams-Sonoma, Inc. (WILLIAMS SONOMA)':None,
+	'Williams International Co LLC (Willc)':None,
+	'Crane & Co., Inc. (Crane Currency)':None,
+	'Spectrum, Inc. (Spectrum Laboratories)':None,
+	'Chase International Inc (Chase Intl Dstinctive Property)':None,
+	'Roberts Markel Weinberg Butler Hailey PC':None,
+	'Gap Solutions, Inc.':None,
+	'Loews Hotels Holding Corporation (Loews Hotels & Resorts)':None,
+
 }
+
 
