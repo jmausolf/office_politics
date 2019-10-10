3ba973b7b016a0e8be92f2d4f6d9bc39492cfaa8
jmausolf <jgm346@gmail.com>
2019-10-10 13:22:03 -0500

    prog

--- clean_experiment/clean_bounce_merge.py
+++ clean_experiment/clean_bounce_merge.py
@@ -70,8 +70,6 @@ def extract_email(message):
 			email = email[:-1]
 		else:
 			pass
-		#print(email)
-		#print(email[-1:])
 
 	return email
 
@@ -90,7 +88,6 @@ def ret_email_full_name(row):
 	to_email = row['to_email']
 
 	try:
-		#match = re.search(r'Dear\s\w+\s+\w+\:', message)
 		match = re.search(r"Dear\s[\w'\-,.][^0-9_!¡?÷?¿\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}\:", message)
 		salutation = match.group(0)
 		full_name = salutation.split('Dear ')[1].split(':')[0]
@@ -102,17 +99,12 @@ def ret_email_full_name(row):
 		first_name = None
 		last_name = None
 
-		
-	#print(full_name)
 	contact_email = lookup_email_fn(first_name, last_name, to_email, ex)
 	return contact_email
 
-	#print(full_name, first_name)
 
 def lookup_email_fn(first, last, to_email, df):
 
-
-	#print(first, last, to_email)
 	crit = (
 				(df['contact_name'] == first) &
 				(df['contact_last_name'] == last) &
@@ -120,17 +112,50 @@ def lookup_email_fn(first, last, to_email, df):
 
 			)
 
-
-
 	df = df.loc[crit].copy()
 	if df.shape[0] >= 1:
 		c = df['contact_email'].values[0]
-		#print(c)
 	else:
 		c = None
-	#return df['contact_email']
-	#print(df)
 	return c
+
+
+def ret_email_company(row):
+
+	message = row['message']
+	to_email = row['to_email']
+
+	try:
+		match = re.search(r"(?<=opportunity at )(.*)(?= and)", message)
+		company = match.group(0)
+	except:
+		company = None
+
+	contact_email = lookup_email_company(company, to_email, ex)
+	return contact_email
+
+
+def lookup_email_company(company, to_email, df):
+
+	if company is None:
+		return None
+	else:
+		crit = (
+					(
+						(df['company'] == company) |
+						(df['company'].str.contains(company))
+					) &
+
+					(df['gmail_user'] == to_email)
+
+				)
+
+		df = df.loc[crit].copy()
+		if df.shape[0] >= 1:
+			c = df['contact_email'].values[0]
+		else:
+			c = None
+		return c
 
 
 def ret_email_domain(row):
@@ -144,7 +169,6 @@ def ret_email_domain(row):
 
 def lookup_email_domain(from_domain, to_email, df):
 
-	print(from_domain, to_email)
 	crit = (
 				(
 					(df['contact_full_domain'] == from_domain) |
@@ -156,7 +180,6 @@ def lookup_email_domain(from_domain, to_email, df):
 			)
 
 	df = df.loc[crit].copy()
-	print(df)
 	if df.shape[0] >= 1:
 		c = df['contact_email'].values[0]
 	else:
@@ -187,67 +210,65 @@ def filter_bounces_merge(df):
 
 	#Get Bounces DF
 	df['extracted_email'] = df['message'].apply(extract_email)
-	#print(df)
 
-	#Still Missing
+	#Need to Overwrite Profile Emails with None, Search Using Other Methods
+	df['isprofile'] = df['extracted_email'].apply(isprofile)
+	df.loc[(df['isprofile'] == True), 'extracted_email'] = None
+
+	#Still Missing, Found
 	df_m = df.loc[df['extracted_email'].isna()]
-
-	#Found Emails
 	df1 = df.dropna(subset=['extracted_email'])
-	print(df1.shape)
-
 
 	#Lookup Missing Emails Using Extracted Full Names
 	df2 = df_m.copy()
 	df2['extracted_email'] = df2.apply(ret_email_full_name, axis=1)
-	#print(df2)
-	print(df2.shape)
 
-	#Still Missing
+	#Still Missing, Found
 	df_m = df2.loc[df2['extracted_email'].isna()]
-	print(df_m.shape)
-
-	#Found Emails
 	df2 = df2.dropna(subset=['extracted_email'])
 
 	#Backfill Bounces Sent to Incorrect (but valid addresses)
 	df3 = df_m.copy()
 	df3['extracted_email'] = df3.apply(fill_bounce_email_legit, axis=1)
 
-	#Still Missing
+	#Still Missing, Found
 	df_m = df3.loc[df3['extracted_email'].isna()]
-	print(df_m.shape)
-
-	#Found Emails
 	df3 = df3.dropna(subset=['extracted_email'])
 
 	#Try Looking Up Using From Domain and To Email
 	df4 = df_m.copy()
-	print(df4.columns)
 	df4['extracted_email'] = df4.apply(ret_email_domain, axis=1)
 
-	#Still Missing
+	#Still Missing, Found
 	df_m = df4.loc[df4['extracted_email'].isna()]
+	df4 = df4.dropna(subset=['extracted_email'])
 	print(df_m.shape)
 
-	#Found Emails
-	df4 = df4.dropna(subset=['extracted_email'])
+	#Try Looking Up Using Company and To Email
+	df5 = df_m.copy()
+	df5['extracted_email'] = df5.apply(ret_email_company, axis=1)
 
+	#Still Missing, Found
+	df_m = df5.loc[df5['extracted_email'].isna()]
+	df5 = df5.dropna(subset=['extracted_email'])
+	print(df_m.shape)
 
 	#Bounces All
-	dfb = pd.concat([df1, df2, df3, df4, df_m])
+	dfb = pd.concat([df1, df2, df3, df4, df5, df_m])
 	dfb['isprofile'] = dfb['extracted_email'].apply(isprofile)
 
-	dfb.to_csv("bounce_test2.csv")
+	dfb.to_csv("extracted_bounce_emails.csv")
 	print(dfb.shape)
-	print(dfb.columns)
+	#print(dfb.columns)
 
 
 	#Some 15 are still left, just manually code them
+	#before finding these, try merging with the other code base
 
+	#see if bounces appear in found id's, if so, drop pair or no?
 
 filter_bounces_merge(mb)
-print(ex.columns)
+#print(ex.columns)
 
 
 
